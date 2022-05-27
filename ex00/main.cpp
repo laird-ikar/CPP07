c936c67182917c8c03845866d5534c4bc117ae36
laird-ikar <bguyot@student.42mulhouse.fr>
2022-05-27 09:25:35 +0200

    ex00 done ? but code in header is sus

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,14 +6,25 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 08:20:24 by bguyot            #+#    #+#             */
-/*   Updated: 2022/05/27 08:20:53 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/05/27 09:21:42 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include <iostream>
 
 int main(void)
 {
-	/* code */
+	std::cout << "min(66, 42): " << min<int>(66, 42) << std::endl;
+	std::cout << "min(95984894, 42.42): " << min<double>(95984894, 42.42) << std::endl;
+	std::cout << "max(' ', '*'): " << max<char>(' ', '*') << std::endl;
+	float	s1 = 42.42f;
+	float	s2 = -42.42f;
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << "s2: " << s2 << std::endl;
+	swap<float>(s1, s2);
+	std::cout << "swaaap" << std::endl;
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << "s2: " << s2 << std::endl;
 	return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,6 +6,28 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 08:21:00 by bguyot            #+#    #+#             */
-/*   Updated: 2022/05/27 08:21:02 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/05/27 09:17:09 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+template <class T>
+void	swap(T &a, T &b)
+{
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <class T>
+T	min(T a, T b)
+{
+	return (a < b ? a : b);
+}
+
+template <class T>
+T	max(T a, T b)
+{
+	return (a > b ? a : b);
+}
